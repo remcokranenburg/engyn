d0d0018b3553effb9365e969d0e02788d3301ddc
Remco Kranenburg <remco@burgsoft.nl>
2017-01-25 16:35:45 +0100

    Add image and rand crates as dependencies

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,9 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #[macro_use] extern crate glium;
+extern crate image;
 extern crate nalgebra;
+extern crate rand;
 
 mod teapot;
 
