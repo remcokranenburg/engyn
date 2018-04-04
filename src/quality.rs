e2ec57bdd5d57226ea38249e9813e627840112dc
Remco Kranenburg <remco@burgsoft.nl>
2018-04-04 11:27:37 +0200

    Added commented out get_target_quality function

--- src/quality.rs
+++ src/quality.rs
@@ -129,4 +129,73 @@ impl Quality {
       *self.weight_lod.borrow()
     }
   }
+
+  /*
+  pub fn get_target_quality(&self, performance: &FramePerformance, benchmark: Benchmark)
+      -> [f32; 3] {
+    let weights = vec![
+      *self.weight_resolution.borrow(),
+      *self.weight_msaa.borrow(),
+      *self.weight_lod.borrow(),
+    ];
+
+    let mut candidates = benchmark.get_entries_by_normalized_weights(weights);
+    candidates.sort_unstable_by(|a, b| {
+      if a.draw_time < b.draw_time {
+        Ordering::Less
+      } else if a.draw_time == b.draw_time {
+        Ordering::Equal
+      } else {
+        Ordering::Greater
+      }
+    });
+
+    // Idle time is calculated by taking the target frame time and subtracting pre-draw, draw and
+    // post-draw time. Our ideal idle time is 10% of the target frame time.
+
+    let ideal_idle_time = 0.1 * performance.get_target_frame_time();
+
+    let actual_idle_time = performance.get_idle();
+    let target_frame_time = performance.get_target_frame_time();
+    let actual_idle_ratio = actual_idle_time / target_frame_time;
+
+    // We will compare the actual drawing time to the target drawing time. If they don't match, the
+    // mutliplier is updated. This multiplier converts the benchmark times to the current times,
+    // since we use the benchmark to try to hit the target drawing time.
+
+    let actual_draw_time = performance.get_last_draw_time();
+    let target_draw_time = xxx;
+    let multiplier = actual_draw_time / target_draw_time;
+
+    // We then consider whether we *would* have reached 10% idle time if the target drawing time
+    // *were* achieved. If the answer is no, we should update our target drawing time, because
+    // apparently the pre-draw and post-draw work took a different amount of time than anticipated.
+
+    let simulated_idle_time = actual_idle_time + actual_draw_time - target_draw_time;
+    let simulated_idle_ratio = simulated_idle_time / target_frame_time;
+
+    let diff_idle_time = ideal_idle_time - simulated_idle_time;
+
+    target_draw_time -= diff_idle_time;
+
+    // TODO: compare benchmarked time vs actual time to calculate a multiplier
+
+    let mut previous_candidate: Option<BenchmarkEntry> = None;
+
+    for candidate in candidates {
+      if let Some(previous) = previous_candidate {
+        if previous.draw_time <= predicted && candidate.draw_time > predicted {
+          return candidate.target_quality;
+        }
+      } else if candidate.draw_time > predicted {
+        // if first candidate is already too expensive, return 0 quality
+        return [0.0, 0.0, 0.0];
+      }
+
+      previous_candidate = Some(candidate);
+    }
+
+    // if last candidate was too easy, just return full quality
+    [1.0, 1.0, 1.0]
+  }*/
 }
